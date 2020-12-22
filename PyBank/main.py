f8dd31ad15d14d37e783aca54acc15d92cf82079
Kristen Lega <kristenlega@gmail.com>
2020-12-21 20:19:53 -0600

    Adding more code and analysis file for PyBank assignment.

--- PyBank/main.py
+++ PyBank/main.py
@@ -62,12 +62,26 @@ with open(budgetdata_csv,'r') as csvfile:
 # Calculate the average change
 avgchange = totalchange / (rowcount - 1)
 
-
+# Print the results to the terminal
 print("Financial Analysis")
 print("--------------------")
 print(f'Total Months: {monthcount}')
 print(f'Total: {total}')
-# Decimal precision format found here: https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
+# Decimal precision format citation: https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
 print(f'Average Change: {avgchange:.2f}')
 print(f'Greatest Increase in Profits: {increasemonth} {increase}')
 print(f'Greatest Decrease in Profits: {decreasemonth} {decrease}')
+
+
+# Create a new text file and print the results there
+results = open('analysis/PyBank_Results.txt', "w")
+
+# Write the results - new line formatting citation: https://www.kite.com/python/answers/how-to-write-to-a-file-in-python#:~:text=Use%20writelines()%20to%20write,be%20in%20a%20single%20line.
+results.write(
+    "Financial Analysis" "\n"
+    "--------------------" "\n"
+    f'Total Months: {monthcount}' "\n"
+    f'Total: {total}' "\n"
+    f'Average Change: {avgchange:.2f}' "\n"
+    f'Greatest Increase in Profits: {increasemonth} {increase}' "\n"
+    f'Greatest Decrease in Profits: {decreasemonth} {decrease}')
